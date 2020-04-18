8ce544bc4533034d7816a5831dd569b40d2ba206
BarriWi5 <barri@wi5.io>
2020-04-18 19:38:32 +0100

    Allow OSC scene switcher to change crossover time

--- app/pixelator/osc_switcher.rb
+++ app/pixelator/osc_switcher.rb
@@ -22,13 +22,17 @@ class OscSwitcher
   end
 
   def on_receive(message)
-    scene_name = message.to_a[0]
-    message "#{message.ip_address}:#{message.ip_port} requests '#{scene_name}'"
-    pixelator.load_scene scene_name
+    first_args = message.to_a[0].split(' ')
+    scene_name = first_args[0]
+    if (crossfade = first_args[1])
+      pixelator.load_scene scene_name, crossfade: crossfade.to_i
+    else
+      pixelator.load_scene scene_name
+    end
+    message "Loaded Scene: #{scene_name} (from #{message.ip_address}:#{message.ip_port})"
   rescue Errno::ENOENT
-    message 'Scene not found!'
+    message "Sorry bud, I don't know how to #{scene_name}"
   end
 
   attr_reader :pixelator, :server, :server_ip, :server_port, :osc_address
-
 end
